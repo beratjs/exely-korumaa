ffe8a01a0ae06dbf7d5a93e23e289dcedb790b22
Glitch (classic-ludicrous-work) <none>
2020-05-25 15:31:35 +0000

    🎥🍎 Checkpoint ./komutlar/oto.js:13534429/1470 ./komutlar/otorol.js:13534429/2518 ./komutlar/ototag.js:13534429/1125

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,47 +1,27 @@
 const Discord = require('discord.js');
-const db = require('quick.db');
-const ayarlar = require('../ayarlar.json');
-
-exports.run = async(client, message, args) => {
+const db = require('quick.db')
+const ayarlar = require('../ayarlar.json'),
+      prefix = ayarlar.prefix
+exports.run = async(client, message, args) =>{
   
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
-
-  if (!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send(`**Hey Sen** Evet Sen! Bu Komut İçin Yeterli Yetkin Yok!`)
-  if (!args[0]) return message.channel.send(`Bunumu Arıyorsun? ${prefix}reklam-engel aç/kapat`)
-   
-  if (args [0] == 'aç') {
-   db.set(`otokanal_${message.guild.id}`, kanal.id);
-   db.set(`otorol_${message.guild.id}`, role.id);
-    let otorolcu = await db.fetch(`reklamengel_${message.guild.id}`)
-    
-    const reklamengelcim = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription('**Reklam Engeli Açtım**')
-    return message.channel.send(reklamengelcim)
-
-  }
+let frenzy_ibrahim = await db.fetch(`Frenzy?Code?OtorolRol_${message.guild.id}`) || await db.fetch(`Frenzy?Code?OtorolKanal_${message.guild.id}`)
+if(frenzy_ibrahim) return message.reply(`:x: Bu sistem zaten aktif durumda. Kapatmak için **${prefix}otorolkapat**`)
+let frenzy_role = message.mentions.roles.first()
+let frenzy_kanal = message.mentions.channels.first()
+if(!frenzy_kanal || !frenzy_role) return message.reply(`Otorol sistemini ayarlamak için **rol ve kanal** belirtmelisin.`)
   
-  if (args [0] == 'kapat') {
-      
-db.delete(`otokanal_${message.guild.id}`, kanal.id);
-  db.delete(`otorol_${message.guild.id}`, role.id);
-   const küfürengelcim22 = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription('**Reklam Engeli Kapattım**')
-    return message.channel.send(küfürengelcim22)
-  }
-
-  
-  
-  
-};
+db.set(`Frenzy?Code?OtorolRol_${message.guild.id}`,frenzy_role.id) 
+db.set(`Frenzy?Code?OtorolKanal_${message.guild.id}`,frenzy_kanal.id)
+message.channel.send(`Başarıyla Tamamlandı Yeni Kullanıcılara <@&${frenzy_role.id}> rolünü vereceğim.`)
+};  
 exports.conf = {
- enabled: true,
- guildOnly: false,
-  aliases: ['reklam-engel'],
- permLevel: 0
+  enabled: true, 
+  guildOnly: false, 
+  aliases: ['otorolayarla'],
+  permLevel: 0 
 };
-
 exports.help = {
- name: 'reklam-engelle'
+  name: 'otorol',
+  description: 'Otorol Sistemi - Frenzy Code',
+  usage: 'otorol rol kanal'
 };
