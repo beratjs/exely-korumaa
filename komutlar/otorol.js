024849e484b2bd0b216b6f99c83e6aee8730570e
Glitch (classic-ludicrous-work) <none>
2020-05-25 15:23:37 +0000

    🍉👂 Checkpoint ./komutlar/otorol.js:13534429/4111

--- komutlar/otorol.js
+++ komutlar/otorol.js
@@ -1,47 +1,55 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-const ayarlar = require('../ayarlar.json');
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+          if(db.fetch(`bakim`)) return message.channel.send('<a:tamir:708594410467885057> Size Daha İyi Hizmet Vermek İçin Bakımdayız \n <a:yesbe:707495628112134145> En Yakın Zamanda Tekrar Kodlarımızı Sunacağız')
 
-exports.run = async(client, message, args) => {
-  
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
-
-  if (!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send(`**Hey Sen** Evet Sen! Bu Komut İçin Yeterli Yetkin Yok!`)
-  if (!args[0]) return message.channel.send(`Bunumu Arıyorsun? ${prefix}reklam-engel aç/kapat`)
-   
-  if (args [0] == 'aç') {
-   db.set(`otokanal_${message.guild.id}`, kanal.id);
-   db.set(`otorol_${message.guild.id}`, role.id);
-    let otorolcu = await db.fetch(`reklamengel_${message.guild.id}`)
-    
-    const reklamengelcim = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription('**Reklam Engeli Açtım**')
-    return message.channel.send(reklamengelcim)
-
-  }
-  
-  if (args [0] == 'kapat') {
-      
-db.delete(`otokanal_${message.guild.id}`, kanal.id);
-  db.delete(`otorol_${message.guild.id}`, role.id);
-   const küfürengelcim22 = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription('**Reklam Engeli Kapattım**')
-    return message.channel.send(küfürengelcim22)
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  if (!message.member.hasPermission("MANAGE_ROLES")) {
+    const embed = new Discord.RichEmbed()
+      .setDescription(`<a:hayrbe:707495526114787328> Bu Komut İçin \`Rolleri Yönet\` Yetkisine Sahip Olmalısın!`)
+      .setColor("RANDOM")
+.setFooter(bot.user.username, bot.user.avatar    message.channel.send(embed);
+    return;
+URL)
   }
 
-  
-  
-  
-};
-exports.conf = {
- enabled: true,
- guildOnly: false,
-  aliases: ['reklam-engel'],
- permLevel: 0
+  let role =
+    message.mentions.roles.first() ||
+    message.guild.roles.find(rol => rol.name === args[0]);
+  let kanal = message.mentions.channels.first();
+  if (!role) {
+    return message.channel.send(
+      new Discord.RichEmbed()
+        .setDescription(`<a:hayrbe:707495526114787328> **Bir  Rol Belirt Lütfen!**`)
+        .setColor("RANDOM")
+    );
+  }
+  if (!kanal) {
+    return message.channel.send(
+      new Discord.RichEmbed()
+        .setDescription(`<a:hayrbe:707495526114787328> **Kanal Belirt Lütfen!**`)
+        .setColor("RANDOM")
+    );
+  }
+  const embed = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setDescription(`<a:4568_aVerified:704749531924922399> **Otorol Başarıyla Ayarlandı Kanal; ${kanal}\nRolü; ${role}** `)
+  message.channel.send(embed);
+
+  db.set(`otokanal_${message.guild.id}`, kanal.id);
+  db.set(`otorol_${message.guild.id}`, role.id);
 };
 
-exports.help = {
- name: 'reklam-engelle'
+module.exports.conf = {
+  aliases: ["otorol-ayarla","otorol"],
+  permLevel: 0,
+  enabled: true,
+  guildOnly: true,
+  kategori: "moderasyon"
+};
+
+module.exports.help = {
+  name: "oto-rol",
+  description: "oto-rol",
+  usage: "oto-rol"
 };
