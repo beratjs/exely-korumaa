f63e1b080752612a74d4a5c59e45a7149cf6bcca
Glitch (classic-ludicrous-work) <none>
2020-05-27 12:18:44 +0000

    🎭🌑 Checkpoint ./bot.js:13534429/1157 ./komutlar/hg-bb.js:13534429/2974

--- komutlar/hg-bb.js
+++ komutlar/hg-bb.js
@@ -1,18 +1,9 @@
 const Discord = require('discord.js'),
-db = require('quick.db')
-const ayarlar = require('../ayarlar.json')
+      db = require('quick.db')
 module.exports.run = async (client, message, args) => {
- 
-let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
+          if(db.fetch(`bakim`)) return message.channel.send('<a:tamir:708594410467885057> Size Daha İyi Hizmet Vermek İçin Bakımdayız \n <a:yesbe:707495628112134145> En Yakın Zamanda Tekrar Kodlarımızı Sunacağız')
 
 let channelfc= message.mentions.channels.first()
-if (!args[0])  {
-    const küfürcu0k = new Discord.MessageEmbed()
-    .setTitle('Başarısız')
-    .setDescription(`Kanal Belirtmen Lazım!`)
-      return message.channel.send(küfürcu0k)
-
-  }   
 if (!message.member.hasPermission("BAN_MEMBERS")) {
     const embed = new Discord.RichEmbed()
       .setDescription(`**Bu Komutu Kullanmak İçin \`Üyeleri At\` Yetkisine Sahip Olmalısın**`)
@@ -23,11 +14,7 @@ if (!message.member.hasPermission("BAN_MEMBERS")) {
   }
 if(!channelfc) return message.reply('**<a:hayrbe:707495526114787328>Lütfen Bir Kanal Etiketleyiniz!**')
 db.set(`FrenzyResimsizHGBB_${message.guild.id}`,channelfc.id)
- const küfürengelcim23 = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription('Karşılama Sistemini  Açtım')
-    return message.channel.send(küfürengelcim23)
-
+message.reply('**<a:ee:707495562944970783> Başarıyla Sistem Ayarlandı!**')
 }
 exports.conf = {
   enabled: true,
@@ -37,7 +24,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'hg-bb-aç',
+  name: 'hg-bb',
   description: 'Frenzy Code',
   usage: 'Frenzy Code'
 };
