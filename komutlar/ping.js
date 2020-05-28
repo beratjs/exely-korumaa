58d0cbcd364bbb149dadcd77e544ae6aa3d1fc07
Glitch (classic-ludicrous-work) <none>
2020-05-28 06:56:06 +0000

    🍓🐢 Checkpoint ./komutlar/ping.js:13534429/828

---
+++ komutlar/ping.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+exports.run = async(client, message ) => {
+
+  message.channel.send(` $client.ws.ping}`)
+};
+exports.conf = {
+  enabled: true,  
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+exports.help = {
+  name: 'pingim'g}; 
