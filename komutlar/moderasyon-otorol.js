6944f58c0f6c5dc6cc676980f8decf04b76616a5
Glitch (classic-ludicrous-work) <none>
2020-05-30 10:17:15 +0000

    🌴🚲 Checkpoint ./komutlar/moderasyon-otorol.js:13534429/1687 ./komutlar/otorol.js:13534429/9

--- komutlar/moderasyon-otorol.js
+++ komutlar/moderasyon-otorol.js
@@ -2,15 +2,29 @@ const Discord = require('discord.js')
 const db = require('quick.db')
 const ayarlar = require('../ayarlar.json')
 
-exports.run = async(client, message, args,) => {
+exports.run = async(client, message, args) => {
 
-  let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
+  let p = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
  let otorol = message.mentions.roles.first()
  let ototakipkanal = message.mentions.channels.first()
+ if (!args[0]) {
+  const sa = new Discord.MessageEmbed()
+  .setTitle('Hatalı Kullanım!')
+  .setDescription(`Bunumu Arıyorsun? ${p}otorol @rol #kanal`)
+  return message.channel.send(sa)
+}
  if (!otorol) {
    const rol = new Discord.MessageEmbed()
-   .setTitle('B')
+   .setTitle('Başarısız!')
+   .setDescription(`Rol Belirtmen Lazım!`)
+   return message.channel.send(rol)
  }
+if (!ototakipkanal) {
+  const kanal = new Discord.MessageEmbed()
+  .setTitle('Başarısız!')
+  .setDescription(`Kanal Belirtmen Lazım!`)
+  return message.channel.send(kanal )
+}
 
 
 }
