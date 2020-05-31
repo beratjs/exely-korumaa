06f390d519f9673c17ebd2417d3f6db71908e676
Glitch (classic-ludicrous-work) <none>
2020-05-31 13:29:12 +0000

    💓👼 Checkpoint ./komutlar/modlog.js:13534429/362 ./komutlar/reklam-egnel.js:13534429/2762

--- komutlar/modlog.js
+++ komutlar/modlog.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js')
+const db = require('quick.db');
+const ayarlar = require('../ayarlar.json');
+
 
 exports.run = async(client, message, args) => {
+  
+let p = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
 
 if (!args[0]) {
   const sa = new Discord.MessageEmbed()
-  .setTitle('Başarısız')
+  .setTitle('Hatalı Kullanım!')
+  .setDescription(`Bunumu Arıyorsun? ${p}modlog aç/kapat`)
+  return message.channel.send(sa)
 }
 };
 exports.conf = {
