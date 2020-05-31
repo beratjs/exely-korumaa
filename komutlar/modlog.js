7c763f6509fc1e3fedd5dc9e8871c6697df5fd78
Glitch (classic-ludicrous-work) <none>
2020-05-31 13:41:18 +0000

    🌓🛢 Checkpoint ./komutlar/küfür-engel.js:13534429/2892 ./komutlar/modlog.js:13534429/916

--- komutlar/modlog.js
+++ komutlar/modlog.js
@@ -6,13 +6,40 @@ const ayarlar = require('../ayarlar.json');
 exports.run = async(client, message, args) => {
   
 let p = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
-
+if (!message.member.permissions.has('KICK_MEMBERS')) {
+    const izinyok = new Discord.MessageEmbed()
+    .setTitle('Başarısız')
+    .setDescription('Bu Komut İçin Yetkin Yok!')
+    return message.channel.send(izinyok)
+}
 if (!args[0]) {
   const sa = new Discord.MessageEmbed()
   .setTitle('Hatalı Kullanım!')
   .setDescription(`Bunumu Arıyorsun? ${p}modlog aç/kapat`)
   return message.channel.send(sa)
 }
+  if (args [0] == 'aç') {
+    db.set(`modlog_${message.guild.id}`, 'açık')
+    let modlogbyme = await db.fetch(`modlog_${message.guild.id}`)
+    
+    const küfürengelcim = new Discord.MessageEmbed()
+    .setTitle('Başarılı')
+    .setDescription('Modlogu Açtım')
+    return message.channel.send(küfürengelcim)
+
+  }
+  
+  if (args [0] == 'kapat') {
+    
+    db.delete(`modlog_${message.guild.id}`)
+
+   const küfürengelcim2 = new Discord.MessageEmbed()
+    .setTitle('Başarılı')
+    .setDescription('Modlogu Kapattım')
+    return message.channel.send(küfürengelcim2)
+   
+  }
+
 };
 exports.conf = {
   enabled: true,  
