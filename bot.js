fe3ae8b0d1e0e65e8c68fb7ab195c9ae750fa30c
Glitch (rowan-fabulous-crawdad) <none>
2020-06-01 11:22:41 +0000

    🏂🎳 Checkpoint ./bot.js:13534429/26 ./komutlar/moderasyon-otorol.js:5334798/3361:13534429/901

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js');//sa marabalar
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
--- komutlar/moderasyon-otorol.js
+++ komutlar/moderasyon-otorol.js
@@ -1,57 +1,41 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require('discord.js');
+const db = require('quick.db');
 const ayarlar = require('../ayarlar.json')
+exports.run = async(client, msg, args) => {
+    let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
 
-exports.run = async(client, message, args) => {
-
-  let p = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
-  let heheü = await db.fetch(`otorol_${message.guild.id}`) || await db.fetch(`ototakipkanal_${message.guild.id}`)
-
- let otorol = message.mentions.roles.first()
- let ototakipkanal = message.mentions.channels.first()
- if (!args[0]) {
-  const sa = new Discord.MessageEmbed()
-  .setTitle('Hatalı Kullanım!')
-  .setDescription(`Bunumu Arıyorsun? ${p}otorol aç @rol #kanal`)
-  return message.channel.send(sa)
-}
-  if(heheü) {
-    const heheücüm = new Discord.MessageEmbed()
-    .setTitle('Başarısız')
-    .setDescription(`Görünüşe Göre Otorol Zaten Açık!`)
-    return message.channel.send(heheücüm)
+  //üşendin dim i qedapgoaskpaıgjsdfapsıdjgamsdofaksmdfewq
+  if (!msg.member.permissions.has('MANAGE_ROLES')) {
+    const yetkiyok = new Discord.MessageEmbed()
+    .setTitle('Başarısız!')
+    .setDescription(`Bunumu Arıyorsun? !otorol`)
   }
-
- if (!otorol) {
-   const rol = new Discord.MessageEmbed()
-   .setTitle('Başarısız!')
-   .setDescription(`Rol Belirtmen Lazım!`)
-   return message.channel.send(rol)
- }
-if (!ototakipkanal) {
-  const kanal = new Discord.MessageEmbed()
-  .setTitle('Başarısız!')
-  .setDescription(`Kanal Belirtmen Lazım!`)
-  return message.channel.send(kanal)
+  
+  if (args[0] == 'ayarla') {
+    var rol1;
+    var rol2;
+    
+    let otoRol = msg.mentions.roles.first() || msg.guild.roles.get(args.join(' '));
+    
+    if (!otoRol) return msg.channel.send('Rol etiketlesene aq')
+    
+    else rol1 = msg.mentions.roles.first().id
+    
+    let rolIsim = msg.mentions.roles.first().name
+    let otoKanal = msg.mentions.channels.first();
+    
+  }
 }
-  if (args [0] == 'aç') {
-    db.set(`otorol_${message.guild.id}`,otorol.id)
-    db.set(`ototakipkanal_${message.guild.id}`,ototakipkanal.id)
-    let lu = await db.fetch(`otorol_${message.guild.id}`)
-    const otorols = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription(`Otorolü Ayarladım \n Verilecek Rol : <@&${otorol.id}> Olarak Ayarladım`)
-    return message.channel.send(otorols)
-    }
 
-}
 exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: ['otorol'],
-    permLevel: 0
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
 }
 
 exports.help = {
-    name: 'otorol'
+  name: 'otorol',
+  description: 'ayberk abin otorol yaptı ewqewwq',
+  usage: 'otorol yapıyon işte sadpgoaks bunlara gerek yok usage description genellıkle siliyorumdlf'
 }
