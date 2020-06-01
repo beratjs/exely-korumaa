fa25756079dc6cc8d7effceec13a9e8b804c2f2c
Glitch (rowan-fabulous-crawdad) <none>
2020-06-01 17:45:38 +0000

    😋🏹 Checkpoint ./komutlar/moderasyon-duyuru.js:13534429/4571

--- komutlar/moderasyon-duyuru.js
+++ komutlar/moderasyon-duyuru.js
@@ -1,32 +1,43 @@
 const Discord = require('discord.js');
 
-exports.run = function (client, message, args) {
-    let kanal = message.mentions.channels.first();
-    let duyurumetni = args.join(" ").slice(22);
-    if(!kanal) return message.channel.send("<a:693873043021365280:700003254259548192> Hata bir kanal etiketlemelisini!");
-  if(!duyurumetni) return message.channel.send("<a:693873043021365280:700003254259548192> Hata duyuru metni yazmalısınız!");
-  message.delete();
-  message.channel.send("<a:77ba453a8bc94763bc49e6a663fd509d:700003210747838525> Başarıyla duyuruldu!");
-    kanal.createWebhook("Rays Duyuru", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYMTbkB9kOpUs75-FNXeqPMMMXmXhdD22Rb4VXRtVKyrUWgnHz")
-    .then(webhook => webhook.edit("Rays Duyuru", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYMTbkB9kOpUs75-FNXeqPMMMXmXhdD22Rb4VXRtVKyrUWgnHz")
-        .then(wb => {
-            const duyurucuPing = new Discord.WebhookClient(wb.id, wb.token);
-            duyurucuPing.send(duyurumetni + "\n")
-            duyurucuPing.delete()
-        })
-        .catch(console.error))
-        .catch(console.error);
+ exports.run = (client, message, args) => {
+    message.delete();
+
+
+   
+   let question = args.join(' ');
+
+   let user = message.author.username
+
+   if (!question) return message.channel.send(
+   new Discord.MessageEmbed()
+    .setTitle('Hatalı!')
+     .setDescription('Neyi Oylayacaksın?')).then(m => m.delete(5000));
+        message.channel.send(
+
+       
+       message.channel.send( `**${question}**`)).then(function(message) {
+
+     
+           setTimeout(()=> {
+
+
+
+
+},240000).then.channel.send('Süre Bitti!')
+        });
+      
+       
+ };
+
+     exports.conf = {
+       enabled: true,
+       guildOnly: false,
+      aliases: ['duyruru'],
+  permLevel: 2
 };
 
-module.exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
+exports.help = {
+  name: 'duyuru'
 };
-
-module.exports.help = {
-  name: 'duyuru',
-  description: 'Duyurucu ping',
-  usage: 'duyuru'
-};
+     
