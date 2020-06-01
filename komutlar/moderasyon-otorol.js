c7bed95067d467ff7c64e9b8fcb50711417b23ab
Glitch (rowan-fabulous-crawdad) <none>
2020-06-01 11:33:49 +0000

    ✨🎫 Checkpoint ./komutlar/moderasyon-otorol.js:5334798/929:13534429/126

--- komutlar/moderasyon-otorol.js
+++ komutlar/moderasyon-otorol.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 const db = require('quick.db');
 const ayarlar = require('../ayarlar.json')
 exports.run = async(client, msg, args) => {
-    let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
+    let p = await require('quick.db').fetch(`prefix_${msg.guild.id}`) || ayarlar.prefix
 
   //üşendin dim i qedapgoaskpaıgjsdfapsıdjgamsdofaksmdfewq
   if (!msg.member.permissions.has('MANAGE_ROLES')) {
     const yetkiyok = new Discord.MessageEmbed()
     .setTitle('Başarısız!')
-    .setDescription(`Bunumu Arıyorsun? !otorol`)
+    .setDescription(`Bunumu Arıyorsun? ${p}otorol`)
   }
   
   if (args[0] == 'ayarla') {
@@ -17,12 +17,22 @@ exports.run = async(client, msg, args) => {
     
     let otoRol = msg.mentions.roles.first() || msg.guild.roles.get(args.join(' '));
     
-    if (!otoRol) return msg.channel.send('Rol etiketlesene aq')
+    if (!otoRol) return msg.channel.send('>:( rol etıketle yoksa duvucem')
     
     else rol1 = msg.mentions.roles.first().id
     
     let rolIsim = msg.mentions.roles.first().name
     let otoKanal = msg.mentions.channels.first();
+    
+    if(!otoKanal) return msg.channel.send("Bi tane kanal etiketlicen yoksa nereye atıcam o mesajları?")
+    
+    db.set(`otorolIsim_${msg.guild.id}`, rolIsim)
+    
+    let rolKanal = await db.set(`otorolKanal_${msg.guild.id}`, msg.mentions.channels.first().id)
+
+    let otorol = await db.set(`otorol_${msg.guild.id}`, rol1)
+    if(!msg.guild.roles.get(rol1)) return msg.channel.send("Etiketlenen rolü bulamadım mk. Düzgün etiketle şunu")
+    msg.channel.send("Otorol sistemi hazır..")
     
   }
 }
