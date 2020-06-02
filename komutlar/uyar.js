2f7fbe16afc28a4de5537d4b01ef6f829c2fb206
Glitch (rowan-fabulous-crawdad) <none>
2020-06-02 10:12:36 +0000

    🍝🎭 Checkpoint ./komutlar/uyar.js:13534429/2370

---
+++ komutlar/uyar.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js')
+const db = require('quick.db')
+exports.run = async(client, message, args) => {
+
+let user = message.mentions.users.first()
+let sebep = args.slice(1).join(' ')
+
+if (!(user||sebep)) return message.reply('Kişi ve sebep belirt')
+let uyarısayı = await db.fetch(`UyarıKullanıcı_${message.guild.id}_${user.id}`)
+
+if (!uyarısayı) {
+db.set(`UyarıKullanıcı_${message.guild.id}_${user.id}`,1)
+message.reply(`Başarıyla ${user.username} ı ${sebep} i ile Uyardınız! Kullanıcının Uyarı Sayısı 1!`)
+user.send(`${message.guild.name} Sunucusundan ${sebep} i ile Uyarı Aldınız! Bu İlk Uyarın`).catch(err=> {})
+} else {
+
+
+db.set(`UyarıKullanıcı_${message.guild.id}_${user.id}`,1)
+
+db.add(`UyarıKullanıcı_${message.guild.id}_${user.id}`,1)
+message.reply(`Kullanıcı Uyarıldı! Başarıyla ${user.username} ı ${sebep} i ile Uyardınız.Toplamda ${uyarısayı} kadar uyarısı var!`)
+user.send(`${message.guild.name} Sunucusundan ${sebep} i ile Uyarı Aldınız! Toplam Uyarın ${uyarısayı}`).catch(err=> {})
+}
+
+if (uyarısayı > 5) {
+
+user.kick(sebep)
+message.reply('Kullanıcı Sunucudan Kicklendi Çünkü Uyarı Sayısı 5 oldu!')
+db.delete(`UyarıKullanıcı_${message.guild.id}_${user.id}`)
+user.send(`${message.guild.name} Sunucusundan ${sebep} i ile Uyarı Aldınız! Toplam Uyarın ${uyarısayı} olduğu için Sunucudan Atıldın`)
+}
+};
+exports.conf = {
+enabled: false,
+guildOnly: false,
+aliases: [],
+permLevel : 0
+};
+exports.help = {
+name: 'uyar'
+};
