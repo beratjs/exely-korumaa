d9ba432d71406df37089abbc7b503f3c11881e86
Glitch (rowan-fabulous-crawdad) <none>
2020-06-02 14:20:33 +0000

    🏊🏼💋 Checkpoint ./komutlar/gelişmiş-yardım.js:13534429/1053

--- komutlar/gelişmiş-yardım.js
+++ komutlar/gelişmiş-yardım.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js')
-const db = require('quick.db')
-const ayarlar = require('../ayarlar.json')
 
 exports.run = async(client, message) => {
-    let prefix = await require('quick.db').fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
-
+  
     const y = new Discord.MessageEmbed()
-    .addField('Moderasyon', '`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`')
+    .addField('**MODERASYON**','`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`')
+    .addField('**KULLANICI**','`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`')
+    .addField('**EĞLENCE**','`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`,`bişikoy`')
+    .setFooter(`${client.user.username}`, client.user.avatarURL)
+    //yandakı cıkan resım aktıf etmek ıstersenız .setThumbnail(client.user.avatarURL) eger yapacaksın bu bolumu ve acıklamayı sılın ve // bunuda
     return message.channel.send(y)
 };
 exports.conf = {
