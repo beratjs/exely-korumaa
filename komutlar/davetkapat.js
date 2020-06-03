00ff5bfeb7383b5e39113e2b27c32c122d66f4f9
Glitch (rowan-fabulous-crawdad) <none>
2020-06-03 07:22:02 +0000

    ⛵️🚣 Checkpoint ./komutlar/davettakip.js:13534429/1073 ./komutlar/davetkapat.js:13534429/2331

--- komutlar/davetkapat.js
+++ komutlar/davetkapat.js
@@ -15,27 +15,10 @@ if (!message.member.permissions.has('MANAGE_SERVER')) {
   if (!args [0]) {
     const sv = new Discord.MessageEmbed()
     .setTitle('Başarısız')
-    .setDescription(`Bunumu Arıyorsun? \n ${p}davettakipayarla `)
+    .setDescription(`Bunumu Arıyorsun? \n ${p}davet-takip kapat `)
     return message.channel.send(sv)
   }
-  if (!kanal) {
-       const sv2 = new Discord.MessageEmbed()
-    .setTitle('Başarısız')
-    .setDescription(`Kanal Belirtmeyi Unuttun!`)
-    return message.channel.send(sv2)
-  }
-  if  (args [0] == 'aç') {
-    db.set(`davettakip_${message.guild.id}`,kanal.id)
-    let lu2 = await db.fetch(`davettakip_${message.guild.id}`)
-    
-
-    const küfürengelcim23 = new Discord.MessageEmbed()
-    .setTitle('Başarılı')
-    .setDescription(`Davet Sistemi Açıldı! \n Kanal : ${kanal} olarak ayarlandı!`)
-    .setColor("RED")
-    return message.channel.send(küfürengelcim23)
-
-  }
+ 
   
   if (args [0] == 'kapat') {
     
@@ -57,5 +40,5 @@ exports.conf = {
   permLevel: 0
 };
 exports.help = {
-  name: 'd'
+  name: 'davet-takip'
 }; 
