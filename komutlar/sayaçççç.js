013aa97b25ed2a4d94f206a9fc9360372a1f40a9
Glitch (sasaqweawe) <none>
2020-06-29 14:10:34 +0000

    🐽⛷ Checkpoint ./komutlar/sayaçççç.js:17406785/1491

--- komutlar/sayaçççç.js
+++ komutlar/sayaçççç.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js')
 const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
+  
+  let sayıcık = await db.fetch(`sayıcık_${message.guild.id}`)
  let kanal = message.mentions.channels.first()
  let hedef = args[1]
     if(!message.member.permissions.has('MANAGE_SERVER')) {
@@ -11,19 +13,31 @@ exports.run = async (client, message, args) => {
         return message.channel.send(a)
       }
 
-
+   
       if (!kanal) {
      return message.reply('Kanal Belirt Dostum!')
-      }
+      } 
       if (!hedef) {
        return message.reply('Sayı Belirt')
       }
-      if (!isNaN(hedef)) {
+      if (isNaN(hedef)) {
           return message.reply('Dostum Hedef Sadece Rakamlardan Oluşabilir')
       }
-      if (hedef > message.guild.memberCount) {
+      if (hedef < message.guild.memberCount) {
      return message.reply('Dostum Üye Sayısından Küçük Sayı Belirtemezssin')
 
+      }
+  if (!sayıcık) {
+  db.set(`sayıcık_${message.guild.id}`,hedef)
+  db.set(`kanalcık_${message.guild.id}`,kanal.id)
+  message.channel.send(`Kanal ${kanal}, Hedef ${hedef} Olarak Ayarlandı`)
+  
+  } else {
+    if (sayıcık) {
+      db.delete(`sayıcık_${message.guild.id}`)
+  db.delete(`kanalcık_${message.guild.id}`)
+      message.channel.send('Sayaç Zaten Ayarlı Olduğu İçin Kapatıldı')
+    }
       }
 }
 exports.conf = {
