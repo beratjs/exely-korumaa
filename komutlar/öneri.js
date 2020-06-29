c34091a5d994463ac43dd8f19847fc351ff4d193
Glitch (sasaqweawe) <none>
2020-06-29 13:25:38 +0000

    🏓🚦 Checkpoint ./komutlar/öneri.js:17406785/614 ./ayarlar.json:17406785/71

---
+++ komutlar/öneri.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const db = require('quick.db');
+
+exports.run = async (client, message, args) => {
+ 
+
+let önericik = args.slice(0).join(' ')
+if (!önericik) return message.channel.send('Heyyy! Öneri Belirtki Atayım')
+
+
+
+const sa = new Discord.MessageEmbed()
+.setTitle('Öneri Var!')
+.setTimestamp()
+
+.setFooter('Öneri Sistemi')
+.setDescription(`Kullanıcı : ${message.author.tag} \n Sunucu : ${message.guild.name} \n Önerisi : ${önericik} `)
+client.channels.cache.get(' ').send(sa)
+                
+}
+exports.conf = {
+  aliases: [],
+  permLevel: 0
+}
+exports.help = {
+  name: "öneri"
+}
