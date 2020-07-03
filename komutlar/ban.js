6d7e1d5f7856e67968c477f8a039ce5b39196bbf
Glitch (sasaqweawe) <none>
2020-07-03 16:20:52 +0000

    🏪👧 Checkpoint ./komutlar/ban.js:17406785/515 ./komutlar/banlimit.js:17406785/386

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -3,21 +3,25 @@ const db = require('quick.db')
 exports.run = async(client, message, args ) => {
   
 let banlanıcak = message.mentions.users.first()
+let banlimit = await db.fetch(`banlimit_${message.guild.id}`)
+ let bansayı= await db.fetch(`bansayı_${message.guild.id}`, 1)
 
 if (!banlanıcak) return message.channel.send(`Kişi Seç!`)
-  
+  if (bansayı > banlimit) return message.channel.send(`b`)
   
   let sebep = args.slice(1).join(' ')
   if (!sebep) return message.channel.send('Sebep Belirt')
   
   
 
-  
+  db.add(`bansayı_${message.author.id}`, 1)
   let kanal = await db.fetch(`banlog_${message.guild.id}`)
   if (kanal) {
     const sa = new Discord.MessageEmbed()
     .setTitle('Kullanıcı Banlandı!')
-    .setDescription(``)
+    .setDescription(` ${banlanıcak} Adlı Kullanıcı **${sebep}** Yüzünden <@${message.author.id}> Tarafından Sunucudan Yasaklandı`)
+    .setTimestamp()
+    client.channels.cache.get(kanal).send(sa)
   }
   
 
