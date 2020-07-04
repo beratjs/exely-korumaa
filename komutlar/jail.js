cbba4128e9f50cd60b173151cb2d1852d18bc397
Glitch (succulent-misty-spectacles) <none>
2020-07-04 16:43:26 +0000

    🚞🍍 Checkpoint ./komutlar/jail.js:17406785/335

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -8,6 +8,11 @@ let sebep = args.slice(1).join(' ')
  if (!k) return message.channel.send(`Kullanıcı Belirt`)
     if (!sebep) return message.channel.send('Sebep Belirt')
 
+  
+  
+  k.roles.add(jailrol)
+  k.send(`${message.guild.name} Sunucusundan ${sebep} sebebinden dolayı jail yedin!`)
+ message.guild.owner.send(`${k} Adlı Kullanıcı ${message.guild.name} Sunucusundan ${sebep} sebebinden dolayı ${message.author.tag} tarafından jail yedi`)
 };
 exports.conf = {
   aliases: [],
