e437f11b4854228cfa7fac09afd050b80afef278
Glitch (carpal-shy-stetson) <none>
2020-07-06 07:36:58 +0000

    🍐🐄 Checkpoint ./komutlar/uyar.js:17406785/368

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -6,7 +6,10 @@ let a = message.mentions.users.first()
 let sebep = args.slice(1).join(' ')
 
 if (!a || !sebep) return message.channel.send(`Kişi Ve Sebep Belirt`)  
-
+a.send()
+  message.guild.owner.send(` `)
+  
+  
 };
 exports.conf = {
   aliases: [],
